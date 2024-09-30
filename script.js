2d60f795f67120d25b2aaa8c6027e9dde6497b9d
mjsenpaii <matiningmj850@gmail.com>
2024-10-01 00:19:27 +0800

    katamad

---
+++ script.js
@@ -0,0 +1,576 @@
+const departmentTeachers = {
+    cba: [
+        "Verna Liza L. Capiña",
+        "Donabel Magahis",
+        "Ronalyn Motol",
+        "Rhoda Molbog",
+        "Generoso Udanga",
+        "Alfred Ofrecio III",
+        "Carlo Magno Castro",
+        "Jenyza Dimacali-Rojo",
+        "Michael Capiña",
+        "Merryrose Palma",
+        "Gerald Gutierrez",
+        "Marife R. Madla",
+        "Lynle Mendoza",
+        "Michael Jaye Ribleza",
+        "Margarette Guilmo",
+        "Jun Joseph Refuerzo",
+        "Joan Marie Eclipse",
+        "Gilbert Privado",
+        "Glynis Karen N. Raza",
+        "Ann Jhea Mae Lamarca",
+        "Azael May Q. Peñaroyo",
+        "Ronalyn Villalva",
+        "Mark Louie Tanz Galang",
+        "Rechille Ricafrente",
+        "Jeanie May Palatino",
+        "Jeimyleen Angeli Cas",
+        "Angelica Mheg Cañazares",
+        "Suyen Chi",
+        "Eljohn Cipriano",
+        "Vince Corcuera",
+        "Analisa Largado",
+        "Gelacio Lauresta",
+        "Alchona Wayne Maigting",
+        "Hazel Manao",
+        "Denchel Minga",
+        "Grace Camille Nabos",
+        "Sophia Yvete Orilla",
+        "Carell Sadiwa",
+        "Nereeze Angela Soleta",
+        "Roche Zoleta",
+        "Atty. Kimberly Rose Loto, CPA",
+        "Amiel Sharil B. Salvador, CPA",
+        "Diana Jean G. Ricafranca, CPA",
+        "Mae Kristine Montaron, CPA",
+        "Glenn Magadia, CPA",
+        "Jonald Binaluyo, CPA",
+        "Jhomarie Puente, CPA",
+        "Atty. Loisa Rosario Reyes",
+        "Atty. Marc Ronald Lavarez",
+        "Atty. Shermela C. Estipona",
+        "Fiscal Lea Angeliza Rioflorido",
+        "Dhanna Kate De Sena",
+        "David Apigo (SC)",
+        "Mirla Virtucio (SC)",
+        "Rannel Perez",
+        "Calvin Russ V. Par",
+        "Arnold Basas",
+        "Danna Diovinda T. Magahis, CPA",
+        "Rocelynne Fernandez Tagla, CPA",
+        "El Bryan Mainit",
+        "Abby Larosa"
+    ],
+    cics: [
+        "Benedicto, Wilbert P.",
+        "Borja, Doreena Joy C.",
+        "Go, Villi Dane M.",
+        "Imperio, Lorimer L.",
+        "Jasmin, Kevin H.",
+        "Logmao, Beneden M.",
+        "Magcamit, Art Jervin L.",
+        "De Luna-Malinao, Eunice G.",
+        "Malinao, Ronjie Mar L.",
+        "Marapia, Jeffrey M.",
+        "Mascareñas, Robert M.",
+        "Nabos, Christine May L.",
+        "Nabos, Juliet Q.",
+        "Rebistual, Christopher J.",
+        "Requiz, Aldrin R.",
+        "Solomon, Maria Nessa M.",
+        "Villaruel, Kierven A.",
+        "Aguiflor, Ivy Joy N.",
+        "Alojado, Carmelo O.",
+        "Carpio, Melvin M.",
+        "Estrella, Maria Jeanne P.",
+        "Fellizar, Iveelyn L.",
+        "Jinao, Melvin M.",
+        "Josue, James Allen M.",
+        "Lagar, Carl Ian S.",
+        "Landig, Raymart M.",
+        "Laylay, Abegail Joy L.",
+        "Mandalihan, Jayvi M.",
+        "Mangui, Meynard O.",
+        "Rey, Aldrin R.",
+        "Omiping, Kenneth L.",
+        "Ornedo, Jonex M.",
+        "Ortega, Recalla M.",
+        "Pascual, Wilmer M.",
+        "Podaca, Jet Francis Q.",
+        "Rejano, Coney Marie O.",
+        "Relatorres, Joshua M.",
+        "Rey, Pollux M.",
+        "Romana, Clarissa P.",
+        "Suarez, Dhomalin M.",
+        "Valenzuela, Mark Carlo"
+    ],
+    educ: [
+        "Dr. Joy S. Montejo",
+        "Dr. Emmanuel L. Asuncion",
+        "Dr. Leodegario M. Jalos, Jr.",
+        "Dr. Liza Marie M. Manoos",
+        "Dr. Susan B. Pineda",
+        "Mrs. Maria Victoria M. Miciano",
+        "Dr. Diomenes L. Montaron",
+        "Dr. Noel R. Palomares",
+        "Mrs. Annie M. Ruiz",
+        "Mrs. Ruby Ann T. Lantita",
+        "Mr. Arnel M. Lantita",
+        "Mr. Raoul J. Magcamit",
+        "Mrs. Mae Kristine C. Ogayre",
+        "Mr. Joshua Aaron M. Pagulayan",
+        "Mr. Genil S. Labaguis",
+        "Mrs. Bernadette Del Prado",
+        "Ms. Sharmaine B. Balut",
+        "Ms. Willa Mae M. Paredes",
+        "Mr. Joseph M. Balatico",
+        "Mrs. Ma. Cyrene P. Lacdao",
+        "Ms. Romalyn M. Macayaon",
+        "Mrs. Clarivel M. Miranda",
+        "Mr. Frannie M. Rodil",
+        "Mrs. Ma. Teresa C. Chavez",
+        "Ms. Jocelyn O. Neri",
+        "Mr. Mariano O. Llave",
+        "Mergiecelyn R. Quezada",
+        "Charisa May R. Fernandez",
+        "Jerome Jara Revidizo",
+        "Annalyn J. Decena",
+        "Amelito R. Zulueta"
+    ],
+    cahs: [
+        "MARVIN P. PLATA, MAN, RN, RM",
+        "ALICE JOY M. AQUINO, MAN, RN",
+        "ABEGAIL O. MAGCAMIT, MAN, RN, RM",
+        "DIVINA R. VITTO, MPA, BCHS, RM",
+        "JOY V. MALILAY, MAN, RN",
+        "MARIE GRACE ANNE M. REYNOSO, MPH, RN",
+        "MARIA JOAN C. OCAMPO, RN, MAN (c)",
+        "HERBERT FELIX A. SEMILLA, PHN, USRN, LPT, DST",
+        "ANNA CZARINA F. PALMA, LPT",
+        "MARK DAVE S. BANDOY, LPT",
+        "BERNADETTE JOYCE C. MATINING",
+        "KEVIN PATRICK LARACAS, LPT",
+        "KIM JUNEL MEDINA, LPT",
+        "JOHN RYAN R. LEAÑO, RN",
+        "APRIL M. MONILLA, MD",
+        "WILLIAM C. BERMAS",
+        "RENZ M. MELENDEZ",
+        "AIDEYZE G. PALOMARES",
+        "VANESSA MAY L. OGAYRE",
+        "JHON MARCO M. HERMOSA",
+        "NADINE MAE CLARISSE M. MANDIA",
+        "EVANGELENE L. LAUGO, RM",
+        "MARICEL L. MARCIANO, RM",
+        "ARYAN MARIE M. BAROÑA, RM",
+        "CLEO DARINA M. HERMENIGILDO, RM",
+        "VANESSA P. PAGALING, RM",
+        "JOHN DEINIEL M. ORNEDO, RM",
+        "REBECCA J. MAGLACAS, RN",
+        "LETICIA A. MAGARARU, RN",
+        "CLAUDE IVAN F. FLORES, RN",
+        "FRETZIE ZARAH P. MELENDEZ, RN",
+        "CHRISTIAN DONG L. MENORCA, RN",
+        "MARIE CHARMAINE D. MATIENZO, RN",
+        "CHRISCIELA JOY D. MATIENZO, RN",
+        "PAUL ANDREI A. LUMIGUID, RN, PhD (c)",
+        "MICHAEL G. PIPIT, RN",
+        "GLAIZA T. PALATINO, RN",
+        "JAY ANNE M. FLORES, RN, LPT",
+        "KATRINA ANNE T. BUSTOS, RN",
+        "SUZZANE M. MENDOZA, RN",
+        "JUN KERBELL ROSAS REFORMA, RD",
+        "MARIA CORAZON COCO H. WATIWAT, RN",
+        "MICA SHAUN S. LAMAN, RN",
+        "CHRISTIAN M. MANALO, BSI/T",
+        "JOHN CARLO MALAGOTNOT"
+    ],
+    ce: [
+        "Engr. Melito L. Hirondo – Dean",
+        "Engr. Valerie N. Linga-Maraon",
+        "Engr. Froilan P. Ney",
+        "Engr. Willer D. Dayahan",
+        "Engr. Pablito M. Natal",
+        "Engr. Charizza D. Montarin-Sigue",
+        "Engr. Rester S. Jinang",
+        "Engr. Rubicar L. Victorio",
+        "Engr. Don Mark L. De La Cruz",
+        "Engr. Robert N. Lamonte",
+        "Engr. Maria Carissa M. Landoy",
+        "Engr. April V. Mangana",
+        "Engr. Donnavie L. De La Cruz",
+        "Engr. Nilo M. Buenaventura",
+        "Engr. Bryan B. Jaranillo",
+        "Mr. Edgardo R. Laririt",
+        "Engr. Ma. Irene C. Mitante",
+        "Engr. John Reynan J. Montalban",
+        "Engr. Monica P. Romasanta",
+        "Engr. Teodoro M. Ricamara",
+        "Engr. Jestar Neil D. Milambiling",
+        "Engr. Erica M. Ola",
+        "Engr. Bernadette Ramiro",
+        "Engr. Renz Melendrez",
+        "Engr. Antonio Jerold R. Lantoria, Jr.",
+        "Engr. Ana Fe N. Molato",
+        "Engr. Desiree T. Malalad",
+        "Engr. Jan Errol B. Mampusti",
+        "Engr. Bernie M. Osinsao, Jr.",
+        "Engr. Jonah Marie E. Ricamara",
+        "Ms. Khelyn Jane L. Lumutan",
+        "Engr. Aubrey M. Lanot",
+        "Engr. Dharen M. Ola",
+        "Engr. Jayruel P. Rodil",
+        "Engr. Mark Ardie M. Saguid",
+        "Engr. Mark Kenneth P. Mayo",
+        "Engr. Garry John Magararu",
+        "Engr. Glenn Paula Constantino",
+        "Ms. Alea Blance H. Macalood",
+        "Mr. Alberto P. Rodas Jr.",
+        "Mr. Jayvic L. Piguerra",
+        "Ms. Essa R. Quijano",
+        "Ms. Cristhell An L. Leal",
+        "Ms. Jovy M. Dimaayo",
+        "Mr. Jomar J. Dela Rosa",
+        "Ms. Jayne Marianne M. Maac",
+        "Mr. Neomel A. Perin",
+        "Ms. Koleen Carmona",
+        "Dr. Menandro M. Merlin",
+        "Abegail M. Apostol",
+        "Jose Marco L. Miciano"
+    ],
+    cit: [
+        "Adran, Angelica M.",
+        "Aldovino, Andrea",
+        "Babiera, Lea Marie",
+        "Brual, Jose",
+        "Buella, Edel Marie L.",
+        "Cantona, Anna Carina",
+        "Celiz, Stephanie",
+        "De La Rosa, Unice",
+        "Diolanda, Angelu",
+        "Gutierrez, Mary Beverly Jane P.",
+        "Historillo, Jan Fern",
+        "Laman, Anne Kryshel",
+        "Lamarca, Jahmae",
+        "Landoy, Ulysses",
+        "Lanuevo, Jv Ann",
+        "Larracas, Thalia Ysabelle",
+        "Larrosa, Junard",
+        "Laylay, Reanne Aira M.",
+        "Ledesma, Maria Roxan",
+        "Ledesma, Rommel",
+        "Legaspi, Mark",
+        "Ligiut, Jade Julius",
+        "Logmao, Joan",
+        "Logmao, Sunshine",
+        "Mabuti, Melgar",
+        "Macayaon, Ramon DC.",
+        "Magahis, Ercel",
+        "Malagotnot, Ceres Janet",
+        "Malagotnot, Julius",
+        "Malilom, Dayanara",
+        "Malinao, Rudolph John",
+        "Mangana, Lovely",
+        "Mangana, Ryan",
+        "Mantaring, Nicko",
+        "Marang, Mary Joy",
+        "Marquez, Monecca",
+        "Martinez, Randell P.",
+        "Mate, Laila",
+        "Melecia, Darlene Tagle",
+        "Mendoza, Mario",
+        "Mendros, John Romeo",
+        "Mogol, John Carlo",
+        "Moldon, Albert",
+        "Montalban, Kim Ira",
+        "Mulleda, John Benedict",
+        "Orejuila, Eran Jefferson",
+        "Orilla, Rhea Jane",
+        "Postrado, Roberto",
+        "Reyes, Armando",
+        "Reyes, John Paul",
+        "Ricamara, Janet",
+        "Rocha, Aynah N.",
+        "Rodelas, Dominador",
+        "Selva, Dindo",
+        "Sosa, Ricardo",
+        "Villanueva, Fatima",
+        "Adling, Nikka Mae",
+        "Garcia, Bernice Jacob",
+        "Lanot, Ronald",
+        "Malagotnot, Estanislao Jr.",
+        "Mandia, Jorge",
+        "Maripoque, Enrico",
+        "Mascareñas, Mario",
+        "Merlin, Menandro",
+        "Muhi, Meynard",
+        "Nitoral, Cassius",
+        "Oliva, Orlando",
+        "Pacua, Ma. Lorraine",
+        "Pestaño, Rose Ann",
+        "Silangil, Joseph Erwin",
+        "Sto. Domingo, Ernani",
+        "Jaqueca, Angemel",
+        "Lantita, Randy",
+        "Mataac, Romeo",
+        "Mendoza, Sheryl",
+        "Narzoles, Raymart",
+        "Real, John Patrick"
+    ],
+    ces: [
+        "Dr. Evangeline B. Mandia",
+        "Mr. Ronnel C. Nolos, Msc",
+        "Ms. Jeany M. Bugarin, MSc",
+        "Mr. Christian Russel M. Rabi",
+        "Mr. Dan Felder R. Macabata",
+        "Ms. Raissa Nieva",
+        "Ms. Julie Pearl Brion",
+        "Hon. Tino Manrique",
+        "Ms. Elaine M. Lañete"
+    ],
+    cgov_ccje: [
+        "Frankniel Ciane Regio",
+        "Jerick Jornadal",
+        "Katrina De Mesa",
+        "Maricel Go",
+        "Hide Yasu Nakajima",
+        "April Anne Chavez",
+        "Ma. Carmen Alcober",
+        "Anne Grace Labatete",
+        "Bianca Monteagudo",
+        "Maria Go",
+        "Leni Rodriguez",
+        "Diosdado Zulueta",
+        "Romula Malvar",
+        "MRoss Pantoja",
+        "Hilario Elegado",
+        "Joy Profugo",
+        "Jellian Ricafranca",
+        "Ken Mark Palatino",
+        "Alfonso Reynoso",
+        "Aira Perlada",
+        "Wilmer Imperio",
+        // Members ng CCJE
+        "Mr. Jayceron L. Monteagudo",
+        "Dr. Vergel L. Mayo",
+        "Mr. Junn Quirk I. Espiritu",
+        "Dr. Keam Eric L. Malabayabas",
+        "Dr. Rosalyn Dasco",
+
+        // University Lecturers
+        "Mr. Fernan L. Capistrano",
+        "Mr. Allen Laurenz L. Pascua",
+        "Mr. Mervien Kent F. Sapungan",
+        "Ms. Aira Mae P. Perlada",
+        "Ms. Shiela Mae Rejano",
+        "Mr. Renz Melendez",
+        "Atty. Ivy Rioflorido",
+        "Atty. Carlo Rodas",
+        "Mr. Rhaian Magpantay",
+        "Mr. Markiel O. Monreal"
+    ],
+    cass: [
+        "Dr. Ernesto L. Largado",
+    "Mrs. Jholey Rose L. Lancion",
+    "Mr. Jerome L. Lingon",
+    "Mrs. Rosalinda E. Punzalan",
+    "Ms. Jean Grace M. Solina",
+    "Ms. Aubrey Jen T. Matibag",
+    "Mrs. Anna Roffel O. Lozada",
+    "Ms. Cristina S. Atienza",
+    "Dr. Randy T. Nobleza",
+    "Ms. Sheryl M. Podra",
+    "Mrs. Cherie Ann M. Luna",
+    "Mr. Gerald M. Pajanustan",
+    "Mrs. Emelita R. Rodelas",
+    "Dr. Rosalyn J. Dasco",
+    "Ms. Norilie Dela Cruz",
+    "Ms. Ma. Jessica L. Semilla",
+    "Ms. Nonah Bianca L. Lasac",
+    "Ms. Chinee Lyn Marciano",
+    "Ms. Andita Esmediana",
+    "Mrs. May Anne C. Bañaga",
+    "Mr. John Lester Historillo",
+    "Engr. Bernadette Ramiro",
+    "Ms. Lovely M. Morong",
+    "Mr. Aurel M. Landoy",
+    "Janella N. Laderas"
+    ]
+};
+
+
+
+const allDepartments = {
+    cba: departmentTeachers.cba,
+    cics: departmentTeachers.cics,
+    educ: departmentTeachers.educ,
+    cahs: departmentTeachers.cahs,
+    ce: departmentTeachers.ce,
+    cit: departmentTeachers.cit,
+    ces: departmentTeachers.ces,
+    cgov_ccje: departmentTeachers.cgov_ccje,
+    cass: departmentTeachers.cass,
+};
+
+const slotsContainer = document.getElementById('slots');
+const startBtn = document.getElementById('startBtn');
+const winnerList = document.getElementById('winnerList');
+
+// Remove the department select dropdown
+const departmentSelect = document.getElementById('departmentSelect');
+if (departmentSelect) departmentSelect.remove();
+
+function createSlots() {
+    slotsContainer.innerHTML = '';
+    for (let i = 0; i < 20; i++) {
+        const slot = document.createElement('div');
+        slot.className = 'slot';
+        slot.style.background = 'linear-gradient(135deg, ' + getRandomColor() + ', ' + getRandomColor() + ')'; // Set random gradient color for slots
+        slotsContainer.appendChild(slot);
+    }
+}
+
+function playBackgroundAudio() {
+    const bgIdleSound = document.getElementById('bgIdleSound');
+    bgIdleSound.currentTime = 0;
+    bgIdleSound.play().catch(error => {
+        console.error("Error playing background idle sound:", error);
+    });
+}
+
+function startRaffle() {
+    console.log("Starting raffle..."); // Debug log
+
+    createSlots();
+
+    // Get audio elements
+    const drawSound = document.getElementById('drawSound');
+    drawSound.currentTime = 0; // Reset draw sound to start
+    drawSound.play().catch(error => {
+        console.error("Error playing draw sound:", error);
+    });
+
+    const winners = selectWinners();
+    let index = 0;
+    const interval = setInterval(() => {
+        const slots = document.querySelectorAll('.slot');
+        const allParticipants = Object.values(allDepartments).flat();
+        slots.forEach(slot => {
+            const randomName = allParticipants[Math.floor(Math.random() * allParticipants.length)];
+            slot.textContent = randomName; // Show random names
+            slot.style.backgroundColor = getRandomColor(); // Set random color
+        });
+        index++;
+
+        if (index === 30) { // Stop after 30 updates (for faster animation)
+            clearInterval(interval);
+            highlightWinners(winners);
+        }
+    }, 100);
+}
+
+
+
+
+function selectWinners() {
+    const winners = [];
+    const allParticipants = [];
+
+    // Step 1: Select 1 winner from each college
+    Object.keys(allDepartments).forEach(department => {
+        const departmentTeachers = allDepartments[department];
+        const randomIndex = Math.floor(Math.random() * departmentTeachers.length);
+        winners.push({ name: departmentTeachers[randomIndex], department });
+    });
+
+    // Step 2: Select 8 winners from one random college
+    const randomCollegeIndex = Math.floor(Math.random() * Object.keys(allDepartments).length);
+    const selectedCollege = Object.keys(allDepartments)[randomCollegeIndex];
+    const selectedCollegeTeachers = [...allDepartments[selectedCollege]]; // Copy array to avoid mutation
+
+    // Ensure we do not exceed the number of available teachers
+    while (winners.length < 8 && selectedCollegeTeachers.length > 0) {
+        const randomIndex = Math.floor(Math.random() * selectedCollegeTeachers.length);
+        const selectedWinner = selectedCollegeTeachers[randomIndex];
+
+        // Prevent duplicates and ensure max 2 winners from the same department
+        if (!winners.some(w => w.name === selectedWinner) &&
+            winners.filter(w => w.department === selectedCollege).length < 2) {
+            winners.push({ name: selectedWinner, department: selectedCollege });
+            selectedCollegeTeachers.splice(randomIndex, 1); // Remove the selected winner
+        }
+    }
+
+    // Step 3: Collect all participants for random selection
+    Object.values(allDepartments).forEach(departmentArray => {
+        allParticipants.push(...departmentArray);
+    });
+
+    // Step 4: Select 2 additional random winners from all participants
+    while (winners.length < 9) {
+        const randomIndex = Math.floor(Math.random() * allParticipants.length);
+        const randomWinner = allParticipants[randomIndex];
+
+        // Ensure no duplicate winners and max 2 winners from any department
+        if (!winners.some(w => w.name === randomWinner) &&
+            winners.filter(w => w.department === getDepartmentByTeacher(randomWinner)).length < 2) {
+            winners.push({ name: randomWinner, department: getDepartmentByTeacher(randomWinner) });
+        }
+    }
+
+    return winners;
+}
+
+
+function getDepartmentByTeacher(teacherName) {
+    for (const [department, teachers] of Object.entries(allDepartments)) {
+        if (teachers.includes(teacherName)) {
+            return department;
+        }
+    }
+    return '';
+}
+
+function getRandomColor() {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    return randomColor; // Generate a random color
+}
+
+function highlightWinners(winners) {
+    const slots = document.querySelectorAll('.slot');
+    let highlightedCount = 0;
+
+    while (highlightedCount < 9) {
+        let slotIndex;
+        do {
+            slotIndex = Math.floor(Math.random() * slots.length);
+        } while (slots[slotIndex].style.backgroundColor === '#33ce33' || winners.some(w => w.name === slots[slotIndex].textContent)); // Ensure slot is not already a winner
+
+        slots[slotIndex].style.background = 'linear-gradient(135deg, #d4af37, #ffd700, #b8860b)'; // Metallic gold gradient highlight color for winners
+        slots[slotIndex].textContent = winners[highlightedCount].name; // Display the winner's name in the slot
+        highlightedCount++;
+    }
+
+    displayWinners(winners);
+}
+
+function displayWinners(winners) {
+    winnerList.innerHTML = '';
+    winners.forEach((winner, index) => {
+        const li = document.createElement('li');
+        li.textContent = `${winner.name} (${winner.department.toUpperCase()})`; // Display winner's name and department
+
+        li.style.fontSize = '30px';
+        li.style.fontWeight = 'bold';
+        li.style.opacity = '0';
+        li.style.transition = 'opacity 0.5s ease';
+
+        winnerList.appendChild(li);
+        setTimeout(() => {
+            li.style.opacity = '1';
+        }, index * 600);
+    });
+}
+
+startBtn.addEventListener('click', startRaffle);
+window.onload = playBackgroundAudio;
